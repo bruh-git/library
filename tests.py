50696d1d81e13a2fe8ef5fbfbb57dbfffb6bf3db
bruh-git <brunac.cardososilva@gmail.com>
2025-04-30 16:38:09 -0300

    Commit 6: Moving method get_charge from Rental to Book

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
-from model import Book, Rental, Client
+from model import Book, Client, Rental
+
 
 def test_rent_regular_book_short_duration():
     book = Book("Refactoring", Book.REGULAR)
@@ -6,15 +7,13 @@ def test_rent_regular_book_short_duration():
 
     c = Client("Fulano")
     c.add_rental(r)
-    
+
     expected_report = (
-        "Rental summary for Fulano\n"
-        "- Refactoring: 2\n"
-        "Total: 2\n"
-        "Points: 1"
+        "Rental summary for Fulano\n" "- Refactoring: 2\n" "Total: 2\n" "Points: 1"
     )
-    
+
     assert c.statement() == expected_report
+
 
 def test_rent_regular_book_long_duration():
     book = Book("Refactoring", Book.REGULAR)
@@ -22,27 +21,25 @@ def test_rent_regular_book_long_duration():
 
     c = Client("Fulano")
     c.add_rental(r)
-    
+
     expected_report = (
-        "Rental summary for Fulano\n"
-        "- Refactoring: 3.5\n"
-        "Total: 3.5\n"
-        "Points: 1"
+        "Rental summary for Fulano\n" "- Refactoring: 3.5\n" "Total: 3.5\n" "Points: 1"
     )
-    
+
     assert c.statement() == expected_report
+
 
 def test_rent_multiple_regular_books():
     book1 = Book("Refactoring", Book.REGULAR)
     book2 = Book("Clean Code", Book.REGULAR)
-    
+
     r1 = Rental(book1, 1)
     r2 = Rental(book2, 3)
-    
+
     c = Client("Fulano")
     c.add_rental(r1)
     c.add_rental(r2)
-    
+
     expected_report = (
         "Rental summary for Fulano\n"
         "- Refactoring: 2\n"
@@ -50,10 +47,12 @@ def test_rent_multiple_regular_books():
         "Total: 5.5\n"
         "Points: 2"
     )
-    
+
     assert c.statement() == expected_report
 
-from model import Book, Rental, Client
+
+from model import Book, Client, Rental
+
 
 def test_rent_children_book_short_duration():
     book = Book("Peter Pan", Book.CHILDREN)
@@ -61,15 +60,13 @@ def test_rent_children_book_short_duration():
 
     c = Client("Fulano")
     c.add_rental(r)
-    
+
     expected_report = (
-        "Rental summary for Fulano\n"
-        "- Peter Pan: 1.5\n"
-        "Total: 1.5\n"
-        "Points: 1"
+        "Rental summary for Fulano\n" "- Peter Pan: 1.5\n" "Total: 1.5\n" "Points: 1"
     )
-    
+
     assert c.statement() == expected_report
+
 
 def test_rent_children_book_long_duration():
     book = Book("Peter Pan", Book.CHILDREN)
@@ -77,27 +74,25 @@ def test_rent_children_book_long_duration():
 
     c = Client("Fulano")
     c.add_rental(r)
-    
+
     expected_report = (
-        "Rental summary for Fulano\n"
-        "- Peter Pan: 3.0\n"
-        "Total: 3.0\n"
-        "Points: 1"
+        "Rental summary for Fulano\n" "- Peter Pan: 3.0\n" "Total: 3.0\n" "Points: 1"
     )
-    
+
     assert c.statement() == expected_report
+
 
 def test_rent_multiple_children_books():
     book1 = Book("Peter Pan", Book.CHILDREN)
     book2 = Book("ABC", Book.CHILDREN)
-    
+
     r1 = Rental(book1, 1)
     r2 = Rental(book2, 4)
-    
+
     c = Client("Fulano")
     c.add_rental(r1)
     c.add_rental(r2)
-    
+
     expected_report = (
         "Rental summary for Fulano\n"
         "- Peter Pan: 1.5\n"
@@ -105,8 +100,9 @@ def test_rent_multiple_children_books():
         "Total: 4.5\n"
         "Points: 2"
     )
-    
+
     assert c.statement() == expected_report
+
 
 def test_rent_new_release_book_short_duration():
     book = Book("LLMs", Book.NEW_RELEASE)
@@ -114,15 +110,13 @@ def test_rent_new_release_book_short_duration():
 
     c = Client("Fulano")
     c.add_rental(r)
-    
+
     expected_report = (
-        "Rental summary for Fulano\n"
-        "- LLMs: 3\n"
-        "Total: 3\n"
-        "Points: 1"
+        "Rental summary for Fulano\n" "- LLMs: 3\n" "Total: 3\n" "Points: 1"
     )
-    
+
     assert c.statement() == expected_report
+
 
 def test_rent_new_release_book_long_duration():
     book = Book("LLMs", Book.NEW_RELEASE)
@@ -130,27 +124,25 @@ def test_rent_new_release_book_long_duration():
 
     c = Client("Fulano")
     c.add_rental(r)
-    
+
     expected_report = (
-        "Rental summary for Fulano\n"
-        "- LLMs: 6\n"
-        "Total: 6\n"
-        "Points: 2"
+        "Rental summary for Fulano\n" "- LLMs: 6\n" "Total: 6\n" "Points: 2"
     )
-    
+
     assert c.statement() == expected_report
+
 
 def test_rent_multiple_new_release_books():
     book1 = Book("LLMs", Book.NEW_RELEASE)
     book2 = Book("AWS", Book.NEW_RELEASE)
-    
+
     r1 = Rental(book1, 2)
     r2 = Rental(book2, 5)
-    
+
     c = Client("Fulano")
     c.add_rental(r1)
     c.add_rental(r2)
-    
+
     expected_report = (
         "Rental summary for Fulano\n"
         "- LLMs: 6\n"
@@ -158,23 +150,24 @@ def test_rent_multiple_new_release_books():
         "Total: 21\n"
         "Points: 4"
     )
-    
+
     assert c.statement() == expected_report
+
 
 def test_rent_distinct_books_short_duration():
     book1 = Book("Refactoring", Book.REGULAR)
     book2 = Book("Peter Pan", Book.CHILDREN)
     book3 = Book("LLMs", Book.NEW_RELEASE)
-    
+
     r1 = Rental(book1, 1)
     r2 = Rental(book2, 1)
     r3 = Rental(book3, 1)
-    
+
     c = Client("Fulano")
     c.add_rental(r1)
     c.add_rental(r2)
     c.add_rental(r3)
-    
+
     expected_report = (
         "Rental summary for Fulano\n"
         "- Refactoring: 2\n"
@@ -183,23 +176,24 @@ def test_rent_distinct_books_short_duration():
         "Total: 6.5\n"
         "Points: 3"
     )
-    
+
     assert c.statement() == expected_report
+
 
 def test_rent_distinct_books_long_duration():
     book1 = Book("Refactoring", Book.REGULAR)
     book2 = Book("Peter Pan", Book.CHILDREN)
     book3 = Book("LLMs", Book.NEW_RELEASE)
-    
+
     r1 = Rental(book1, 3)
     r2 = Rental(book2, 4)
     r3 = Rental(book3, 2)
-    
+
     c = Client("Fulano")
     c.add_rental(r1)
     c.add_rental(r2)
     c.add_rental(r3)
-    
+
     expected_report = (
         "Rental summary for Fulano\n"
         "- Refactoring: 3.5\n"
@@ -208,5 +202,5 @@ def test_rent_distinct_books_long_duration():
         "Total: 12.5\n"
         "Points: 4"
     )
-    
+
     assert c.statement() == expected_report
